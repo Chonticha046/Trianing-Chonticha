b6063e5a2dcca802be14f775e92c2a5ce8b9325a
Chonticha046 <chontichabanliesrisak@gmail.com>
2024-01-14 19:31:00 +0700

    training-chonticha

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <>
+      <h1>ติดต่อ</h1>
+      <hr />
+      <p><h2>NVC Trianing Center</h2></p>
+      <p>Address: 90 Tesa Rd., Mueang, Nakhon Pathom 73000</p>
+      <p>Tel: 034-252790 </p>
+      <p>E-mail: admin@nvctrianingcenter.com</p>
+    </>
+  );
+}
+
+export default contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h1>หน้าหลัก</h1>
+      <center>
+      <img src="/image/nn.jpg" alt="" srcset=""/>
+      </center>
+      
+    </>
+  );
+}
+
+export default Home
